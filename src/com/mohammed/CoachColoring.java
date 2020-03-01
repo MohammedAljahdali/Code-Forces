f82643c8112c79e8a4d8b0879005e07f48f6d66d
Mohammed Aljahdali <mohammedkhalid4028@gmail.com>
2020-03-01 12:26:27 +0300

    Create CoachColoring.java

---
+++ src/com/mohammed/CoachColoring.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(new File("coach.in"));
+        PrintStream printStream = new PrintStream(new FileOutputStream("output"));
+        int testCases = scanner.nextInt();
+        for (int i = 0; i < testCases; i++) {
+            int n = scanner.nextInt();
+            boolean[][] grid = new boolean[n][n];
+            int rp = scanner.nextInt();
+            int cp = scanner.nextInt();
+            for (int j = 0; j < rp; j++) {
+                int row = scanner.nextInt() - 1;
+                for (int k = 0; k < n; k++) {
+                    grid[row][k] = true;
+                }
+            }
+
+            for (int j = 0; j < cp; j++) {
+                int col = scanner.nextInt() - 1;
+                for (int k = 0; k < n; k++) {
+                    grid[k][col] = true;
+                }
+            }
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    if (!grid[j][k])
+                        count++;
+                }
+            }
+            printStream.println(count);
+        }
+    }
+}
